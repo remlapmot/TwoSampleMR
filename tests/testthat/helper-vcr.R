84340ba2b3e47bb76476cf372503ec02b8ce2b28
Tom Palmer <remlapmot@hotmail.com>
2025-04-17 18:29:35 +0100

    Add filter_sensitive_data argument

--- tests/testthat/helper-vcr.R
+++ tests/testthat/helper-vcr.R
@@ -1,5 +1,6 @@
 library("vcr") # *Required* as vcr is set up on loading
 invisible(vcr::vcr_configure(
+  filter_sensitive_data = list("<<<opengwas_x_test_mode_key>>>" = Sys.getenv('OPENGWAS_X_TEST_MODE_KEY')),
   dir = vcr::vcr_test_path("fixtures")
 ))
 vcr::check_cassette_names()
