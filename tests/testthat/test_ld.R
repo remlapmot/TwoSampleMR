df5a5c90da38913d07e432fc513736242f656f5a
Tom Palmer <remlapmot@hotmail.com>
2025-04-18 06:41:23 +0100

    Use vcr in test_ld tests

--- tests/testthat/test_ld.R
+++ tests/testthat/test_ld.R
@@ -1,16 +1,14 @@
 context("ld")
 
-skip_if_offline()
-skip_if_offline(host = "api.opengwas.io")
 skip_on_cran()
 
+vcr::use_cassette("test_ld_01", {
 # extract some data
-a <- try(extract_instruments("ieu-a-2", clump=FALSE))
-if (class(a) == "try-error") skip("Server issues")
-out <- try(clump_data(a))
-if (class(out) == "try-error") skip("Server issues")
+a <- extract_instruments("ieu-a-2", clump=FALSE)
+out <- clump_data(a)
+})
 
-
+vcr::use_cassette("test_ld_02", {
 test_that("clump", {
   skip_if_not(exists('a'), "a not created in test above")
   skip_if_not(exists('out'), "out not created in test above")
@@ -18,22 +16,23 @@ test_that("clump", {
 	expect_true(nrow(a) > nrow(out))
 	expect_true(nrow(out) > 0)
 })
+})
 
-
+vcr::use_cassette("test_ld_03", {
 test_that("matrix", {
   skip_if_not(exists('out'), "out not created in test above")
-	b <- try(ld_matrix(out$SNP))
-	if (inherits(b, "try-error")) skip("Server issues")
+	b <- ld_matrix(out$SNP)
 	expect_equal(nrow(b), nrow(out))
 	expect_equal(ncol(b), nrow(out))
 })
+})
 
-
+vcr::use_cassette("test_ld_04", {
 test_that("clump multiple", {
-	a <- try(extract_instruments(c("ieu-a-2", "ieu-a-1001"), clump=FALSE))
-	if (class(a) == "try-error") skip("Server issues")
+	a <- extract_instruments(c("ieu-a-2", "ieu-a-1001"), clump=FALSE)
 	out <- clump_data(a)
 	expect_equal(length(unique(a$id.exposure)), length(unique(out$id.exposure)))
+})
 })
 
 test_that("clump local", {
